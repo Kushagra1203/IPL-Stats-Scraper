36d20be0a38b61e76b7c85ec249d70177889ea30
Kushagra <103055700+Kushagra1203@users.noreply.github.com>
2024-06-13 11:36:06 +0530

    Update Batting_pipelines.py

--- IPL/pipelines/Batting_pipelines.py
+++ IPL/pipelines/Batting_pipelines.py
@@ -11,13 +11,13 @@ import pymongo
 class BattingAvgPipeline:
     
     def __init__(self):
-    # Replace 'username', 'password', 'your-cluster-url', 'your-database', and 'your-collection' with your actual values
-    # Connect to MongoDB
-    self.client = pymongo.MongoClient("mongodb+srv://username:password@your-cluster-url")
-    # Create or connect to the database
-    self.db = self.client["your-database"]
-    # Create or connect to the collection
-    self.collection = self.db["your-collection"]
+        # Replace 'username', 'password', 'your-cluster-url', 'your-database', and 'your-collection' with your actual values
+        # Connect to MongoDB
+        self.client = pymongo.MongoClient("mongodb+srv://username:password@your-cluster-url")
+        # Create or connect to the database
+        self.db = self.client["your-database"]
+        # Create or connect to the collection
+        self.collection = self.db["your-collection"]
 
     def process_item(self, item, spider):
         if spider.name != 'batting_avg':
